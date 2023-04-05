0021abdb98523ecf0ac6d6e346a5475d33274b95
spencersaunders45 <spencersaunders@pm.me>
2023-04-04 20:31:15 -0600

    added title

---
+++ siterecon.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from rich import print
+from rich.console import Console
+
+console = Console()
+
+console.print(" ___________   _________   _________   _________     ___________    _________    _________    __________    ____        ___ ", style="bold red")
+console.print("|           | |         | |         | |         |   |   _____   |  |         |  |         |  |          |  |     \     |   |", style="bold red")
+console.print("|     ______| |___   ___| |___   ___| |    _____|   |  |     |  |  |    _____|  |    _____|  |   ____   |  |      \    |   |", style="bold red")
+console.print("|    |            | |         | |     |   |         |  |_____|  |  |   |        |   |        |  |    |  |  |       \   |   |", style="bold red")
+console.print("|    |______      | |         | |     |   |_____    |       ____|  |   |_____   |   |        |  |    |  |  |        \  |   |", style="bold red")
+console.print("|           |     | |         | |     |         |   |       \      |         |  |   |        |  |    |  |  |    |\   \ |   |", style="bold red")
+console.print("|______     |     | |         | |     |    _____|   |   |\   \     |    _____|  |   |        |  |    |  |  |    | \   \|   |", style="bold red")
+console.print("       |    |     | |         | |     |   |         |   | \   \    |   |        |   |        |  |    |  |  |    |  \       |", style="bold red")
+console.print(" ______|    |  ___| |___      | |     |   |_____    |   |  \   \   |   |_____   |   |_____   |  |____|  |  |    |   \      |", style="bold red")
+console.print("|           | |         |     | |     |         |   |   |   \   \  |         |  |         |  |          |  |    |    \     |", style="bold red")
+console.print("|___________| |_________|     |_|     |_________|   |___|    \___\ |_________|  |_________|  |__________|  |____|     \____|", style="bold red")
