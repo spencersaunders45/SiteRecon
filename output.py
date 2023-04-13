8a44744e808e067c047ace1ce8e0c587df8bf195
spencersaunders45 <sau18010@byui.edu>
2023-04-13 10:56:36 -0600

    working on output report

--- output.py
+++ output.py
@@ -1,14 +1,25 @@
+import datetime
+
 """
 todo: Add header to output. Default header is the main URL and time it was scanned
+todo: add ability to customize output file name
 """
 
 class Writer:
-    def __init__(self):
-        pass
+    def __init__(self, file_name="siterecon_output.txt"):
+        self.file_name = file_name
 
-    def write_header(self, url, time):
-        f = open("siterecon_output.txt", "a")
-        f.write(f"================= {url} [{time}] =================\n")
+    def write_header(self, url):
+        time_data = datetime.datetime.now()
+        date = time_data.strftime("%x")
+        time = time_data.strftime("%X")
+        f = open(self.file_name, "a")
+        f.write(f"================= {url} [{time} {date}] =================\n")
         f.close()
 
-    def 
+    def write_log(self, text):
+        f = open(self.file_name, "a")
+        f.write(text + "\n")
+        f.close()
+
+Writer().write_header("byui.edu")
