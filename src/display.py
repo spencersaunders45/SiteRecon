7db0364040228ed8c6e3b0f528c9c267f9cc9296
Spencer Saunders <spencersaunders@pm.me>
2023-06-17 21:57:06 -0600

    adding argparser

--- src/display.py
+++ src/display.py
@@ -23,19 +23,23 @@ class IO:
     |__________| |__________|     |__|     |_________|   |___|    \___\ |_________|  |_________|  |__________|  |____|     \____|
     """
 
+
     def __init__(self, crawl_max):
         self.crawl_max = crawl_max
         self.progress = Progress()
         self.progress_bar = self.progress.add_task("Scanning", total=crawl_max, start=False, visible=True)
 
+
     def display_title(self) -> None:
         """Display the application title"""
         console.print(self.title, style="bold red")
+
 
     def get_command(self) -> str:
         """Gets the input from the user"""
         command = input("> ")
         return command
+
 
     def status_report_bad(self, status_code: int, url: str) -> None:
         """Reports a bad site status code 
@@ -51,6 +55,7 @@ class IO:
         """
         console.log(f"[[bold red]{status_code}[/bold red]]: [blue underline]{url}[/blue underline]")
 
+
     def status_report_good(self, status_code: int, url: str) -> None:
         """Reports a good status code
         
@@ -65,6 +70,7 @@ class IO:
         """
         console.log(f"[[bold green]{status_code}[/bold green]]: [blue underline]{url}[/blue underline]")
 
+
     def input_field_found(self, url: str) -> None:
         """Reports url pages where input fields are found
         
@@ -77,61 +83,6 @@ class IO:
         """
         console.log(f"[[bold green]✓[/bold green]]: [blue underline]{url}[/blue underline]")
 
-    def invalid_c_flag(self) -> None:
-        """Reports the improper use of the -c flag"""
-        console.log(f"[[bold red]x[/bold red]]: max wait or min wait is not a number.")
-
-    def invalid_filename(self, file_name: str) -> None:
-        """Reports an invalid filename
-        
-        Parameters:
-        file_name : str
-            The name of the output file
-
-        Returns:
-            None
-        """
-        console.log(f"[[bold red]x[/bold red]]: {file_name} cannot be a filename")
-
-    def invalid_symbol(self) -> None:
-        """Reports a symbol that cannot be used"""
-        console.print('[[bold red]x[/bold red]]: the following symbols cannot be used in a filename [. \ / " * | ? : > <]')
-
-    def invalid_aggression_parameter(self, flag: str) -> None:
-        """Reports an invalid use of the -Dag flag
-        
-        Parameters:
-        flag : str
-            The parameter send with the -Dag flag
-
-        Returns:
-            None
-        """
-        console.print(f'[[bold red]x[/bold red]]: {flag} is not a valid parameter for -Dag')
-
-    def invalid_path(self, path: str) -> None:
-        """Reports and invalid output path
-        
-        Parameters:
-        path : str
-            The given folder path
-
-        Returns:
-            None
-        """
-        console.print(f"[[bold red]x[/bold red]]: {path} is not a valid path")
-
-    def not_a_number(self, value: str) -> None:
-        """Reports that a value is not a number
-        
-        Parameters:
-        value : str
-            The value that is not a number
-
-        Returns:
-            None
-        """
-        console.print(f'[[bold red]x[/bold red]]: {value} is not a number')
 
     def update_progress(self, url: str, count: int) -> None:
         """Displays the progress of the search
@@ -147,33 +98,6 @@ class IO:
             self.progress.start_task(self.progress_bar)
         self.progress.update(self.progress_bar, advance=1)
 
-    def display_help(self) -> None:
-        """Displays the flags"""
-        message = """
-        Aggressiveness:
-            -A: There is no pause between requests. Requests are made as fast as possible.
-            -M: There is a 5 to 15 second pause between requests.
-            -P: There is a 15 to 30 second pause between requests.
-            -C [max wait] [min wait]: This is a custom wait time where you choose the maximum and minimum wait times.
-
-        Output Files:
-            -fn [filename]: Allows you to customize the file name.
-            -fp [output path]: Changes the output path of the file.
-                • The given file path must be the absolute file path.
-
-        Change defaults:
-            -Dag [A/M/P/C] ['C only' max wait] ['C only' min wait]: Change the default aggression type.
-            -Dfn [filename]: Changes the default file name.
-            -Dfp [output path]: Changes the default output path.
-            -Ds [number]: Changes the default number of sites searched.
-
-        Sites searched:
-            -s [number]: Changes the number of sites searched. Default is 20.
-
-        Default settings:
-            -D: Displays the default settings.
-        """
-        console.print(message, style="grey")
 
     def display_defaults(self) -> None:
         """Reports that a settings.json file was not found"""
--- src/siterecon.py
+++ src/siterecon.py
@@ -1,352 +1,95 @@
-# python libraries
-from time import sleep
-import re
-from random import randint
-# external libraries
-from bs4 import BeautifulSoup
-from requests import get, Response
-from rich.progress import track, Progress
-# project imports
-from display import IO
-from node import Node
-from output import Writer
-
-"""
-//todo: make a Node for link structure
-//todo: make different modes (aggressive, passive, etc)
-    //todo: passive mode - randomizes wait time between requests
-//todo: option to save logs
-todo: save non-domain name links and group them by domain
-//todo: find and save emails
-todo: reports site response status
-//todo: make settings json file
-todo: scan which sites have http
-todo: scan which pages have forms on them
-todo: separate results by status, http protocol, forms on pages
-"""
-
-# soup = BeautifulSoup(page.text, 'html.parser')
-
-class SiteRecon():
-    """Runs the application
-    
-    Attributes:
-    url : str
-        The root URL
-    root : Node
-        The root of the Node
-    all_links : set
-        A set of all collected links with the same given base domain
-    crawl_count : int
-        The number of sites scanned
-    crawl_max : int
-        The max number of sites to scan
-    pause_min : int
-        The minimum time to wait before requesting another site
-    pause_max : int
-        The max time to wait before requesting another site
-    aggression : str
-        The aggression mode 
-    file_name : str
-        The name of the output file
-    headers : str
-        The headers sent in the request
-    """
-    root = None
-    crawl_count = 0
-    crawl_max = 25
-    pause_min = 5
-    pause_max = 13
-    aggression = None
-    file_name = None
-    basic_url = None
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-    all_links = set()
-    external_links = set()
-    all_emails = set()
-    urls_with_forms = set()
-    phone_numbers = set()
-    writer = Writer()
-    io = IO(crawl_max)
-
-    def __inti__(self):
-        pass
-
-    def get_http_response(self, url):
-        """Gets the response data from the URL
-        
-        Parameters:
-        url : str
-            The website url
-
-        Returns:
-            Response
-        """
-        try:
-            r = get(url, headers=self.headers)
-            return r
-        except Exception as e:
-            print(e)
-            return Exception
-
-    def is_external_link(self, link: str):
-        """Checks if the found url shares the base domain of the root url
-        
-        Parameters:
-        link : str
-            The found url on the scanned webpage
-
-        Returns:
-            bool
-        """
-        return self.basic_url in link
-
-    #  Checks if a link is internal or external and adds them to the appropriate list
-    def find_page_links(self, soup: BeautifulSoup, url: str) -> None:
-        """Finds and sorts links found on scanned webpage
-        
-        Parameters:
-        soup : BeautifulSoup
-            The HTML of the scanned webpage
-        url : str
-            The current webpage url
-
-        Returns:
-            list
-        """
-        links = []
-        for a_tag in soup.find_all('a'):
-            # Get the link from the a_tag
-            link = a_tag.get('href')
-            if link == None or len(link) == 0:
-                continue
-            # Skips single slashes and all #
-            if len(link) == 1 and link == "/":
-                continue
-            if link[0] == "#":
-                continue
-            # Create full link if needed
-            if link[0] == "/":
-                link = url + link
-            elif "/" not in link and "." not in link:
-                link = url + "/" + link
-            elif "/" in link and "." not in link and link[0] != "/":
-                link = url + "/" + link
-            # Extract mailto: links
-            if "mailto:" in link:
-                split_link = link.split("mailto:")
-                self.all_emails.add(split_link[1])
-                continue
-            # Extract tel: links
-            if "tel:" in link:
-                tel = link.split("tel:")
-                self.phone_numbers.add(tel[1])
-                continue
-            # Sort links
-            if not self.is_external_link(link):
-                self.external_links.add(link)
-            else:
-                links.append(link)
-        return links
-
-    # Adds children nodes to the parent node
-    def add_children(self, soup: BeautifulSoup, parent: Node) -> None:
-        """Adds links found on the scanned webpage to the parent Node
-        
-        Parameters:
-        soup : BeautifulSoup
-            The parsed html of the webpage
-        parent : Node
-            The parent node
-
-        Returns:
-            None
-        """
-        links = self.find_page_links(soup, parent.url)
-        for link in links:
-            if link not in self.all_links:
-                child = Node(link)
-                parent.add_child(child)
-
-    def check_for_input_fields(self, soup: BeautifulSoup, url: str) -> None:
-        """Checks if the scanned webpage has any input fields
-        
-        Parameters:
-        soup : BeautifulSoup
-            The parsed html of the webpage
-        url : str
-            The webpage url
-
-        Returns:
-            None
-        """
-        input_tags = soup.find_all('input')
-        if len(input_tags) > 0:
-            self.urls_with_forms.add(url)
-
-    def find_emails(self, soup: BeautifulSoup) -> None:
-        """Find the emails in the HTML code
-        
-        Parameters:
-        soup : BeautifulSoup
-            The parsed html of the webpage
-        url : str
-            The webpage url
-
-        Returns:
-            None
-        """
-        regex_email = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-        for text in soup.stripped_strings:
-            emails = re.search(regex_email, text)
-            if emails:
-                email_list = emails.group()
-                self.all_emails.add(email_list)
-
-    def request_pause(self) -> None:
-        """Pauses between requests
-        
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-        if self.pause_max > 0:
-            wait_period = randint(self.pause_min, self.pause_max)
-            sleep(wait_period)
-
-    def scan_page(self, url: str, node: Node) -> None:
-        """Scans the html of the page
-        
-        Parameters:
-        url : str
-            The webpage url
-        node : Node
-            The current node being scanned
-
-        Returns:
-            None
-        """
-        # print("SCANNING: ", url)
-        self.crawl_count += 1
-        self.io.update_progress(url, self.crawl_count)
-        self.request_pause()
-        r = self.get_http_response(url)
-        if r == Exception:
-            raise Exception
-        self.writer.log_internal_urls(url, r.status_code)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        self.add_children(soup, node)
-        self.check_for_input_fields(soup, url)
-        self.find_emails(soup)
-
-    # Goes through the Node in a breadth first search
-    def crawl_site(self, parent: Node) -> None:
-        """Crawls through all the pages of the website
-        
-        Parameters:
-        parent : Node
-            The current node of the web tree
-
-        Returns:
-            None
-        """
-        children = parent.get_children()
-        # exit case
-        if self.crawl_count > self.crawl_max or len(children) == 0:
-            return
-        # Scan each child url
-        for child in children:
-            if self.crawl_count > self.crawl_max:
-                break
-            self.scan_page(child.url, child)
-        # recursively call children nodes
-        for child in children:
-            self.crawl_site(child)
-
-    def validate_command(self, command: str):
-        pass
-
-    def split_commands(self, command: str) -> str:
-        """Turns the command into a list
-        
-        Parameters:
-        command : str
-            The commands entered by the user
-
-        Returns:
-            list
-        """
-        parsed_command = command.split(' ')
-        return parsed_command
-
-    def return_url(self, command_list: list) -> str:
-        """Returns the url from the command list
-        
-        Parameters:
-        command : list
-            The list of commands given by the user
-
-        Returns:
-            str : The target url
-        """
-        url = command_list[0]
-        if 'http' in url or 'https' in url:
-            return url
-        else:
-            return "https://" + url
-
-    def target_url(self) -> None:
-        """Gets the target url from the user and starts the scan
-
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-        command = self.io.get_command()
-        # self.validate_command(command)
-        command_list = self.split_commands(command)
-        target_url = self.return_url(command_list)
-        # Create the root Node for the website tree
-        self.root = Node(target_url)
-
-    def get_basic_url(self) -> None:
-        """
-        
-        """
-        if "https://www." in self.root.url:
-            basic_url = self.root.url.split("https://www.")
-        elif "https://" in self.root.url:
-            basic_url = self.root.url.split("https://")
-        self.basic_url = basic_url[1]
-
-    def run_program(self) -> None:
-        """Starts the process of scanning the website
-        
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-        self.io.display_title()
-        self.target_url()
-        self.get_basic_url()
-        self.writer.write_header(self.root.url)
-        self.all_links.add(self.root.url)
-        # try:
-        #     self.scan_page(self.root.url, self.root)
-        #     self.crawl_site(self.root)
-        # except Exception as e:
-        #     print(e)
-        #     print("A failure occurred")
-        self.scan_page(self.root.url, self.root)
-        self.crawl_site(self.root)
-        self.writer.log_data(self.all_emails, self.external_links, self.urls_with_forms, self.all_links)
+from scanner import SiteRecon
+from argparse import ArgumentParser
 
 
-# Run the program
-SiteRecon().run_program()
+sr = SiteRecon()
+parser = ArgumentParser(
+    prog="siterecon",
+    description="Discover information about a given website.",
+    epilog="Use responsibly."
+    )
+parser.add_argument("url", action="store")
+parser.add_argument(
+    '-c',
+    '--count',
+    type=int,
+    default=20,
+    action="store",
+    metavar="<int>",
+    help="The number of webpages you want to scan."
+)
+aggression = parser.add_argument_group("aggression")
+aggression.description = "Affects the wait time between each request. Default is M."
+aggression.add_argument(
+    '-ag',
+    '--aggression',
+    choices=["A", "M", "P"],
+    help="A: 0s | M: 3s-10s | P: 10s-30s",
+    default="M",
+    action="store"
+    )
+aggression.add_argument(
+    '-ca',
+    '--custom-aggression',
+    type=int,
+    nargs=2,
+    action="store",
+    help="Customize wait time between requests.",
+    metavar=("<max>", "<min>"),
+    default=None
+)
+output = parser.add_argument_group("output")
+output.description = "Options for creating the report file."
+output.add_argument(
+    '-fp',
+    '--file-path',
+    help="Location of where you want the output file.",
+    default=".",
+    action="store",
+    type=str,
+    metavar="<path>"
+)
+output.add_argument(
+    '-fn',
+    '--filename',
+    help="The name of the output file.",
+    type=str,
+    action="store",
+    metavar="<file name>",
+    default="output.txt"
+)
+defaults = parser.add_argument_group("defaults")
+defaults.description = "Change the defaults for all the flags"
+defaults.add_argument(
+    '-Dag',
+    '--default-aggression',
+    help="Change the default mode for the -ag flag.",
+    type=str,
+    action="store",
+    choices=["A", "M", "P"]
+)
+defaults.add_argument(
+    '-Dfn',
+    '--default-filename',
+    type=str,
+    action="store",
+    metavar="<default filename>",
+    help="Change the default filename.",
+)
+defaults.add_argument(
+    '-Dfp',
+    '--default-file-path',
+    type=str,
+    action="store",
+    metavar="<default file path>",
+    help="Change the default file path.",
+)
+defaults.add_argument(
+    '-Dc',
+    '--default-count',
+    type=str,
+    action="store",
+    metavar="<default count>",
+    help="Change the default count.",
+)
+args = parser.parse_args()
